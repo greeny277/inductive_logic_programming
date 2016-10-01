a447365d94ead841036e38665350f4c11281b7cf
Christian Bay <christian.bay@studium.fau.de>
2016-10-01 18:12:44 +0200

    Not far away from the finish line. searchBackground fails at least.

--- src/base_of_knowledge.pl
+++ src/base_of_knowledge.pl
@@ -16,16 +16,68 @@ daughter(X, Y) :- female(X),parent(Y,X).
 
 background(female(nancy),female(helen),female(eve),female(mary),parent(helen,mary),parent(helen,tom),parent(george,mary),parent(tom,eve),parent(nancy,eve)).
 
-rlgg(Background, R) :-
-	Background =.. [_|Predicates],
-	antiUnify(Predicates, Predicates, [], R).
+positives(daughter(mary,helen), daughter(eve, tom)).
 
-antiUnify([H|T], FullList, Akk, R) :-
+rlgg(Background, PositiveExamples, R) :-
+	Background =.. [_|PredEx],
+	PositiveExamples =.. [_|PredPos],
+	generatePairs(PredEx, PredEx, [], PairsBack),
+	generatePairs(PredPos, PredPos, [], PairsPosEx),
+	antiUnifyAll(PairsPosEx, PosAU),
+	antiUnifyAll(PairsBack, BackAU),
+	parseAtoms(PosAU, Atoms),
+	searchBackground(BackAU, Atoms, R).
+
+searchBackground([H|T], Atoms, NewRule) :-
+	parseAtoms([H], BackAtoms),
+	isSublist(BackAtoms, Atoms),
+	searchBackground(T, Atoms, R),
+	append([H], R, NewRule).
+searchBackground([_|T], Atoms, NewRule) :-
+	searchBackground(T, Atoms, NewRule).
+searchBackground(_, _, []).
+
+isSublist([H1|T1], L2) :-
+	memberchk(H1, L2),
+	isSublist(T1, L2).
+isSublist([],_).
+
+
+parseAtoms([H|T], Atoms) :-
+	H =.. [_|HAtom],
+	parseAtoms(T, R),
+	append(HAtom, R, Atoms).
+parseAtoms(_, []).
+
+
+antiUnifyAll([H|T], AUPairs) :-
+	antiUnify(H, AUPair),
+	antiUnifyAll(T, R),
+	addHeader(AUPair, R, AUPairs).
+antiUnifyAll(_, []).
+
+antiUnify(Pair, AUPair) :-
+	arg(1,Pair,ContP1),
+	arg(2,Pair,ContP2),
+	ContP1 =.. [P1Func|P1Args],
+	ContP2 =.. [_|P2Args],
+	concateHelper(P1Args, P2Args, [], Foo),
+	AUPair =.. [P1Func|Foo].
+
+concateHelper([H1|T1], [H2|T2], Akk, R) :-
+	sort([H1,H2|[]], [HS1,HS2|_]),
+	atom_concat(HS1, '_', Tmp),
+	atom_concat(Tmp, HS2, Hr),
+	concateHelper(T1, T2, [Hr|Akk], R).
+concateHelper(_, _, Akk, R) :- reverse(Akk, R).
+
+
+generatePairs([H|T], FullList, Akk, R) :-
 	findPair(H, FullList, [], Pairs),
-	antiUnify(T, FullList, [Pairs|Akk], R_tmp),
+	generatePairs(T, FullList, [Pairs|Akk], R_tmp),
 	flatten(R_tmp, R_tmp2),
 	deletePermutedPairs(R_tmp2, R).
-antiUnify(_,_,Pairs, Pairs).
+generatePairs(_,_,Pairs, Pairs).
 
 findPair(Current, [Current|T], Akk, R) :-
 	findPair(Current, T, Akk, R).
